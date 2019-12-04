25695234a38540391661f2f2fd51cfea47e6ce5b
david-cc <david.caudeli@web-education.net>
2019-12-04 11:49:53 +0100

    fix: reintegrate standardise function in string utils

--- projects/ngx-ode-ui/src/lib/utils/string.ts
+++ projects/ngx-ode-ui/src/lib/utils/string.ts
@@ -1,6 +1,12 @@
+import { removeAccents } from '../helpers/accents.helper';
+
 export function trim(text: string): string {
     if (text && text.length > 0) {
         return text.trim();
     }
     return text;
+}
+
+export function standardise(str: string): string{
+  return removeAccents(str != null ? str : '').toLowerCase().replace('\\s+', ' ');
 }
